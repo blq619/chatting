cd44e5520325537fd0d9bcf9227025a7b618a655
kuknam <blq619@naver.com>
2020-07-13 16:06:22 +0900

    chatting

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
 
 app.set('views', `${__dirname}/public`)
-app.set('view engine','html')
-app.engine('html',require('ejs').renderFile)
-app.use(express.static('src',{extensions: ['html']}))
+app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile)
+app.use(express.static('public', {
+    extensions: ['html']
+}))
 
 
+io.on('connection', (socket) => {
+    console.log('User connected...');
+    socket.on('chatting', (data)=>{
+       io.sockets.emit('chatting',data)
+    })
+})
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
+    console.log(__dirname)
     res.render('index');
 })
 
-app.get('/api/users', (req,res) => {
+app.get('/api/users', (req, res) => {
     const users = [{
         name: 'aaa',
         age: 1
     }];
     res.send(users)
 })
-app.listen(PORT, () => {
-console.log(`server is tunning on ${PORT}`);
-})
+// app.listen(PORT, () => {
+// console.log(`server is tunning on ${PORT}`);
+// })
+http.listen(PORT, ()=> console.log(`listening on localhost:${PORT}`));
